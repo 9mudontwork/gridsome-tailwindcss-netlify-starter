5bfe5243e6f62814bdcefee3da95708874ebe594
Muhammad Imoeb <mhmimoeb@gmail.com>
2019-10-14 02:09:28 +0700

    commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,50 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const postcssPlugins = [tailwind()]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins
+      }
+    }
+  },
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    // https://gridsome.org/plugins/@gridsome/source-filesystem
+    // https://gridsome.org/plugins/@gridsome/transformer-remark
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    }
+  ]
 }
